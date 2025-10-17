caf16011ade96dcaaf14885cbaebe810d6473891
Raghuram G <raghuram.g2024cse@sece.ac.in>
2025-10-17 22:17:20 +0530

    Update WelcomeController.java

--- javadb-project-leap-class-project-Raghuram-G01/javadb-project-leap-class-project-Raghuram-G01/src/main/java/com/raghu/hello/Controller/WelcomeController.java
+++ javadb-project-leap-class-project-Raghuram-G01/javadb-project-leap-class-project-Raghuram-G01/src/main/java/com/raghu/hello/Controller/WelcomeController.java
@@ -1,14 +1,14 @@
-package com.raghu.hello.Controller;
+// package com.raghu.hello.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-@Controller
-public class WelcomeController {
-    @GetMapping("/welcome")
-    public String showWelcomePage(Model model){
-        model.addAttribute("message","Welcome to Spring Boot Application");
-        // Now it correctly points to "hello.html"
-        return "hello"; 
-    }
-}
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// @Controller
+// public class WelcomeController {
+//     @GetMapping("/welcome")
+//     public String showWelcomePage(Model model){
+//         model.addAttribute("message","Welcome to Spring Boot Application");
+//         // Now it correctly points to "hello.html"
+//         return "hello"; 
+//     }
+// }
