9e1a656dda4caf54a633d73dae8f10e05c222181
Raghuram G <raghuram.g2024cse@sece.ac.in>
2025-10-17 22:18:02 +0530

    Update HelloController.java

--- javadb-project-leap-class-project-Raghuram-G01/javadb-project-leap-class-project-Raghuram-G01/src/main/java/com/raghu/hello/Controller/HelloController.java
+++ javadb-project-leap-class-project-Raghuram-G01/javadb-project-leap-class-project-Raghuram-G01/src/main/java/com/raghu/hello/Controller/HelloController.java
@@ -1,78 +1,78 @@
-package com.raghu.hello.Controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-// This annotation indicates that this class is a REST controller get method
-public class HelloController {
-
-    @GetMapping("/hello")
-    // This annotation maps HTTP GET requests to the sayHello method
-    public String sayHello() {
-        return "Hello Raghuram!";
-    }
-}
-
-// code to multiply this 2 numbers
-
-// package com.raghu.hello;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// public class HelloController {
-
-//     @GetMapping("/hello")
-//     public int add(@RequestParam int num1, @RequestParam int num2) {
-//         return num1 + num2;
-//     }
-// }
-
-// code to square of number
-
-// package com.raghu.hello;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// public class HelloController {
-// @GetMapping("/hello")
-// public int square(@RequestParam int num) {
-//     return num * num;
-// }
-// }
-
-// !Day 2
-
 // package com.raghu.hello.Controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RestController;
 
-// import com.raghu.hello.Service.HelloService;
-
-// import org.springframework.web.bind.annotation.RequestParam;
-
 // @RestController
+// // This annotation indicates that this class is a REST controller get method
 // public class HelloController {
 
-//     @Autowired
-//     private HelloService helloService;
-
 //     @GetMapping("/hello")
+//     // This annotation maps HTTP GET requests to the sayHello method
 //     public String sayHello() {
-//         return helloService.sayHello();
+//         return "Hello Raghuram!";
 //     }
-
-//     @GetMapping("/hello/ragu") // ! Define a GET endpoint at /path
-//     public String getMethodName(@RequestParam String param) {
-//         return helloService.getMethodName(param); // ! Call the service method with the provided parameter
-//     }
-
 // }
+
+// // code to multiply this 2 numbers
+
+// // package com.raghu.hello;
+
+// // import org.springframework.web.bind.annotation.GetMapping;
+// // import org.springframework.web.bind.annotation.RequestParam;
+// // import org.springframework.web.bind.annotation.RestController;
+
+// // @RestController
+// // public class HelloController {
+
+// //     @GetMapping("/hello")
+// //     public int add(@RequestParam int num1, @RequestParam int num2) {
+// //         return num1 + num2;
+// //     }
+// // }
+
+// // code to square of number
+
+// // package com.raghu.hello;
+
+// // import org.springframework.web.bind.annotation.GetMapping;
+// // import org.springframework.web.bind.annotation.RequestParam;
+// // import org.springframework.web.bind.annotation.RestController;
+
+// // @RestController
+// // public class HelloController {
+// // @GetMapping("/hello")
+// // public int square(@RequestParam int num) {
+// //     return num * num;
+// // }
+// // }
+
+// // !Day 2
+
+// // package com.raghu.hello.Controller;
+
+// // import org.springframework.beans.factory.annotation.Autowired;
+// // import org.springframework.web.bind.annotation.GetMapping;
+// // import org.springframework.web.bind.annotation.RestController;
+
+// // import com.raghu.hello.Service.HelloService;
+
+// // import org.springframework.web.bind.annotation.RequestParam;
+
+// // @RestController
+// // public class HelloController {
+
+// //     @Autowired
+// //     private HelloService helloService;
+
+// //     @GetMapping("/hello")
+// //     public String sayHello() {
+// //         return helloService.sayHello();
+// //     }
+
+// //     @GetMapping("/hello/ragu") // ! Define a GET endpoint at /path
+// //     public String getMethodName(@RequestParam String param) {
+// //         return helloService.getMethodName(param); // ! Call the service method with the provided parameter
+// //     }
+
+// // }
